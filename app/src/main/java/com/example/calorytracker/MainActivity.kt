7fd9b5ddc2d1a85134074fd8528a70461edcc81f
marat <marakhakim@gmail.com>
2022-12-15 22:13:33 +0100

    Implement Activity and Goal screens

--- app/src/main/java/com/example/calorytracker/MainActivity.kt
+++ app/src/main/java/com/example/calorytracker/MainActivity.kt
@@ -16,8 +16,10 @@ import androidx.navigation.compose.rememberNavController
 import com.example.calorytracker.navigation.navigate
 import com.example.calorytracker.ui.theme.CaloryTrackerTheme
 import com.example.core.navigation.Route
+import com.example.onboarding_presentation.activity.ActivityScreen
 import com.example.onboarding_presentation.age.AgeScreen
 import com.example.onboarding_presentation.gender.GenderScreen
+import com.example.onboarding_presentation.goal.GoalScreen
 import com.example.onboarding_presentation.height.HeightScreen
 import com.example.onboarding_presentation.weight.WeightScreen
 import com.example.onboarding_presentation.welcome.WelcomeScreen
@@ -60,10 +62,10 @@ class MainActivity : ComponentActivity() {
 
                         }
                         composable(Route.ACTIVITY) {
-
+                            ActivityScreen(onNavigate = navController::navigate)
                         }
                         composable(Route.GOAL) {
-
+                            GoalScreen(onNavigate = navController::navigate)
                         }
                         composable(Route.TRACKER_OVERVIEW) {
 
