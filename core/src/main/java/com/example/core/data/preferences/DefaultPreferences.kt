b777259503e677f881d54818636b9073097d67db
marat <marakhakim@gmail.com>
2022-12-22 20:59:26 +0100

    Add TrackerOverviewViewModel

--- core/src/main/java/com/example/core/data/preferences/DefaultPreferences.kt
+++ core/src/main/java/com/example/core/data/preferences/DefaultPreferences.kt
@@ -87,4 +87,14 @@ class DefaultPreferences(
             activityLevel = ActivityLevel.fromString(activityLevelString ?: "keep_weight")
         )
     }
+
+    override fun saveShouldShowOnboarding(shouldShow: Boolean) {
+        sharedPref.edit()
+            .putBoolean(Preferences.KEY_SHOULD_SHOW_ONBOARDING, shouldShow)
+            .apply()
+    }
+
+    override fun loadShouldShowOnboarding(): Boolean {
+        return sharedPref.getBoolean(Preferences.KEY_SHOULD_SHOW_ONBOARDING, true)
+    }
 }
